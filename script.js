a760748da813082ac658710930ac29d856af111d
create-alt <toshikakimika@gmail.com>
2025-01-03 02:24:09 +0900

    add winter task

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function toggleMenu() {
     const menu = document.querySelector('.menu');
     menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
 }
+
+let currentSlide = 0;
+showSlide(currentSlide);
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) currentSlide = 0;
+    if (index < 0) currentSlide = slides.length - 1;
+
+    slides.forEach((slide, i) => {
+        slide.style.display = (i === currentSlide) ? 'block' : 'none';
+    });
+}
+
+function changeSlide(step) {
+    currentSlide += step;
+    showSlide(currentSlide);
+}
